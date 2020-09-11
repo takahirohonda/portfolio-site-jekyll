9202003a47ba774bac28ea852062da2262aca289
mydatahack <thon6thon6@hotmail.com>
2020-09-11 18:14:34 +1000

    check

--- deploy-scripts/add-base-url.js
+++ deploy-scripts/add-base-url.js
@@ -6,3 +6,10 @@ fs.writeFileSync(path.join(__dirname, '../_data/baseUrl.yml'), line);
 
 console.log(`Updated baseUrl successfully`);
 console.log(line);
+
+const baseUrl = 'bseurl: "/portfolio"';
+
+fs.appendFileSync(path.join(__dirname, '../_config.yml'), baseUrl);
+
+console.log('Successfully appended the baseurl to config.yml');
+console.log(baseUrl);
